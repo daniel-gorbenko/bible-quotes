0f60e276409b79e91bcc596e47444004f3e8f710
Daniel Gorbenko <busymangorbenko@gmail.com>
2019-08-12 14:22:19 +0300

    added cors to the api

--- api/route.js
+++ api/route.js
@@ -1,6 +1,10 @@
+let cors = require('cors');
+
 let BooksController = require('./controllers/books_controller.js');
 
 module.exports = (app) => {
+  app.use(cors());
+  
   app.get('/books', BooksController.get);
   app.get('/books/:bookAbbrev/:topic/:verseStart-:verseEnd', BooksController.getVerses);
 }
