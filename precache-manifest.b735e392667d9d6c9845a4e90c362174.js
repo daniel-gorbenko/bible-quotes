60bf209637abef1a254e2bb724df8a41d24bbc7a
Daniel Gorbenko <busymangorbenko@gmail.com>
2019-08-13 15:26:49 +0300

    Updates

---
+++ precache-manifest.b735e392667d9d6c9845a4e90c362174.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eb3142cc5c3b5ee2e8d321e51378f45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "353b0d738902a6e30548",
+    "url": "/static/css/main.93fdb11b.chunk.css"
+  },
+  {
+    "revision": "6f1d1ebd08090806a005",
+    "url": "/static/js/2.1bb75eac.chunk.js"
+  },
+  {
+    "revision": "353b0d738902a6e30548",
+    "url": "/static/js/main.53c4817d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
