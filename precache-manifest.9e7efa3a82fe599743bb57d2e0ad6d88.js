0ef4404668509d04c36b0986775cd895643be550
Daniel Gorbenko <busymangorbenko@gmail.com>
2019-08-13 15:31:09 +0300

    Updates

---
+++ precache-manifest.9e7efa3a82fe599743bb57d2e0ad6d88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2ea9137f7886b9bcc66813c4de32df9",
+    "url": "/bible-quotes/index.html"
+  },
+  {
+    "revision": "353b0d738902a6e30548",
+    "url": "/bible-quotes/static/css/main.93fdb11b.chunk.css"
+  },
+  {
+    "revision": "6f1d1ebd08090806a005",
+    "url": "/bible-quotes/static/js/2.1bb75eac.chunk.js"
+  },
+  {
+    "revision": "353b0d738902a6e30548",
+    "url": "/bible-quotes/static/js/main.53c4817d.chunk.js"
+  },
+  {
+    "revision": "36c51c3ed48524870b7d",
+    "url": "/bible-quotes/static/js/runtime~main.cf540e01.js"
+  }
+]);
