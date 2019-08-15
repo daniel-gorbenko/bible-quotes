298a223e2afb09ad0c51a576cc6261d95cf01665
Daniel Gorbenko <busymangorbenko@gmail.com>
2019-08-15 11:14:33 +0300

    deleted api dir from this repo

--- api/controllers/errors_controller.js
+++
@@ -1,9 +0,0 @@
-class ErrorsController {
-  constructor() {}
-
-  handle(err, request, response, next) {
-    response.send({success: false, error: err.message, data: null})
-  }
-}
-
-module.exports = new ErrorsController();
